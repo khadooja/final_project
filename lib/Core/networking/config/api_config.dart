5a097f69d091b8f4467233962f209da1b0ebcc7b
Khadija <@DESKTOP-FVA8FDT>
2025-05-19 22:21:04 +0300

    ...apir

--- lib/Core/networking/config/api_config.dart
+++ lib/Core/networking/config/api_config.dart
@@ -1,9 +1,27 @@
 import 'package:dio/dio.dart';
+import 'dart:io';
 
+class ApiConfig {
+  static late String baseUrl;
+
+  static void configureDio(Dio dio) {
+    // تحقق هل التطبيق يعمل على Android emulator
+    if (Platform.isAndroid) {
+      baseUrl = 'http://10.0.2.2:8000/api/';
+    } else {
+      baseUrl = 'http://localhost:8000/api/';
+    }
+
+    dio.options.baseUrl = baseUrl;
+    dio.interceptors.add(LogInterceptor());
+  }
+}
+
+/*
 class ApiConfig {
   static const String baseUrl = 'http://10.0.2.2:8000/api/';
   static void configureDio(Dio dio) {
     dio.options.baseUrl = baseUrl;
     dio.interceptors.add(LogInterceptor());
   }
-}
+}*/
